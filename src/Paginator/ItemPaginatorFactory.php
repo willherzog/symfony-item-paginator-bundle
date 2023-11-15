25d44183526ac4a8fa837ff206007b3d6417198b
willherzog <willherzog@gmail.com>
2023-11-15 16:02:17 -0700

    Update ItemPaginatorFactory.php
    Allow second argument of create() method to be omitted, in which case the default configuration will be used

--- src/Paginator/ItemPaginatorFactory.php
+++ src/Paginator/ItemPaginatorFactory.php
@@ -4,14 +4,16 @@ namespace WHSymfony\WHItemPaginatorBundle\Paginator;
 
 use Doctrine\ORM\EntityManagerInterface;
 
-use WHSymfony\WHItemPaginatorBundle\Config\PaginatorConfigBuilder;
+use WHSymfony\WHItemPaginatorBundle\Config\{PaginatorConfigBuilder,PaginatorConfigBuilderFactory};
 
 class ItemPaginatorFactory
 {
-	public function __construct(protected readonly EntityManagerInterface $entityManager)
-	{}
+	public function __construct(
+		protected readonly EntityManagerInterface $entityManager,
+		protected readonly PaginatorConfigBuilderFactory $configBuilderFactory
+	) {}
 
-	public function create(string $class, PaginatorConfigBuilder $configBuilder): ItemPaginator
+	public function create(string $class, PaginatorConfigBuilder $configBuilder = null): ItemPaginator
 	{
 		if( !class_exists($class) ) {
 			throw new \InvalidArgumentException(sprintf('Class "%s" does not exist.', $class));
@@ -19,6 +21,10 @@ class ItemPaginatorFactory
 
 		if( !is_subclass_of($class, ItemPaginator::class, true) ) {
 			throw new \InvalidArgumentException(sprintf('Class "%s" exists but does not extend from "%s".', $class, ItemPaginator::class));
+		}
+
+		if( $configBuilder === null ) {
+			$configBuilder = $this->configBuilderFactory->createBuilder();
 		}
 
 		return new $class($configBuilder->build(), $this->entityManager);
