31df65a5a4999bb507b18c42b7f8e5b5218eef9d
Will Herzog <willherzog@gmail.com>
2025-07-01 17:17:23 -0600

    Extend AbstractRequestQueryFilter instead of using IsApplicableRequestQueryTrait

--- src/Filter/ItemSearchFilter.php
+++ src/Filter/ItemSearchFilter.php
@@ -2,30 +2,29 @@
 
 namespace WHSymfony\WHItemPaginatorBundle\Filter;
 
+use WHSymfony\WHItemPaginatorBundle\Filter\RequestQuery\RequestQueryDefinition;
 use WHSymfony\WHItemPaginatorBundle\Paginator\ItemPaginator;
 use WHSymfony\WHItemPaginatorBundle\Paginator\SearchableItemPaginator;
+
+use WHSymfony\WHItemPaginatorBundle\Exception\InvalidFilterParameterException;
 
 /**
  * @author Will Herzog <willherzog@gmail.com>
  */
-class ItemSearchFilter implements ItemFilter, HasRequestQuery
+class ItemSearchFilter extends AbstractRequestQueryFilter
 {
-	use IsApplicableRequestQueryTrait;
-
 	public const REQUEST_QUERY = 'search';
-
-	protected bool $requireNotEmpty = true;
 
 	public function __construct(protected readonly array $searchColumns)
 	{
 		if( empty($this->searchColumns) ) {
-			throw new \InvalidArgumentException('Please specify at least one search column');
+			throw new InvalidFilterParameterException('Please specify at least one search column.');
 		}
 	}
 
-	public function getRequestQueryName(): string
+	protected function getRequestQueryDefinition(): RequestQueryDefinition
 	{
-		return self::REQUEST_QUERY;
+		return new RequestQueryDefinition(self::REQUEST_QUERY);
 	}
 
 	public function supports(ItemPaginator $paginator): bool
@@ -38,6 +37,6 @@ class ItemSearchFilter implements ItemFilter, HasRequestQuery
 	 */
 	public function apply(ItemPaginator $paginator): void
 	{
-		$paginator->setSearchTermAndColumns($this->requestQueryValue, $this->searchColumns);
+		$paginator->setSearchTermAndColumns($this->getRequestQueryValue(), $this->searchColumns);
 	}
 }
